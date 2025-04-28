e2dc571853bcd7c2dedadbbf1db2f7e445952d0b
Mohammad-Ranjbr <mohammadranjbar.mmr81@gmail.com>
2025-04-28 11:06:10 +0330

    Working With HTTP Headers

--- src/main/java/com/example/practical/api/server/DefaultRestApi.java
+++ src/main/java/com/example/practical/api/server/DefaultRestApi.java
@@ -1,7 +1,9 @@
 package com.example.practical.api.server;
 
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +22,17 @@ public class DefaultRestApi {
     @GetMapping("/time")
     public String time(){
         return LocalDateTime.now().toString();
+    }
+
+    @GetMapping("/header-one")
+    public String headerByAnnotation(@RequestHeader("User-agent") String headerUserAgent, @RequestHeader(value = "Practical-java" , required = false) String headerPracticalJava) {
+        return "User-agent : " + headerUserAgent + " Practical-Java : " + headerPracticalJava;
+    }
+
+    @GetMapping("/header-two")
+    public String headerByRequest(ServerHttpRequest serverHttpRequest) {
+        return "User-agent : " + serverHttpRequest.getHeaders().getFirst("User-agent")
+                + " , Practical-Java : " + serverHttpRequest.getHeaders().getValuesAsList("Practical-java");
     }
 
 }
