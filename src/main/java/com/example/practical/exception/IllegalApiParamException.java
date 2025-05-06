e34bfe2c51383d7a5695bfc06117e75e123ad04e
Mohammad-Ranjbr <mohammadranjbar.mmr81@gmail.com>
2025-05-06 11:45:56 +0330

    API Monitor with Elasticsearch & Kibana

--- src/main/java/com/example/practical/exception/IllegalApiParamException.java
+++ src/main/java/com/example/practical/exception/IllegalApiParamException.java
@@ -4,6 +4,17 @@ import java.io.Serial;
 
 public class IllegalApiParamException extends RuntimeException {
 
+    // Serialization means converting a Java object into a byte stream, so that:
+    // you can store it (for example, in a file or database), or send it over the network (for example, from a client to a server or between microservices).
+    // And deserialization means the opposite: reconstructing the object from the bytes.
+    // Now what is the role of serialVersionUID?
+    // When serializing, Java stores a version number (the same serialVersionUID) with the object. If the class structure changes later (for example, a field is added/removed), Java checks:
+    // If the version number is different, it throws an InvalidClassException error.
+    // So: serialVersionUID guarantees that the version of the stored object is compatible with the current class.
+    // From Java 14 onwards, if you include a field called serialVersionUID in the class, it is better to use the @Serial annotation to indicate that this field is for serialization.
+    // Why is it used in Exception?
+    // In practice, we usually don't serialize exceptions. But since RuntimeException itself is Serializable, for completeness, custom exception classes should also have a serialVersionUID — even if it's not used.
+
     @Serial
     private static final long serialVersionUID = 1L;
 
